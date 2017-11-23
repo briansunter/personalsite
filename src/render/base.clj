3a0e1eae51b08c21f381c9684ae5c9ddf80a02b5
Brian Sunter <bsunter@gmail.com>
2017-11-22 21:42:22 -0800

    Basic post css

--- src/render/base.clj
+++ src/render/base.clj
@@ -1,6 +1,9 @@
 (ns render.base
   (:require [hiccup.core :refer [deftag]]
-            [hiccup.page :refer [html5 include-css include-js]]))
+            [hiccup.page :refer [html5 include-css include-js]]
+            [garden.stylesheet :refer [at-media]]
+            [garden.core :refer [css]]))
+
 (def async-fonts [:script "WebFontConfig = { google: { families:
     ['Source Sans Pro:400,600,700,400italic,700italic',
     'Montserrat',
@@ -12,16 +15,29 @@
       wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
       wf.async = true;
       s.parentNode.insertBefore(wf, s);
-   })(document);"
-  ])
+   })(document);"])
+
+(def style [[:h1 :h2 :h3 :h4 {:font-family "'Monserrat', sans-serif"}]
+            [:ul.social {:display "flex"
+                         :justify-content "space-between"}]
+            [:p {:font-family "'Lora', serif"}]
+            [:div.content {:grid-column " 2 / 23"}]
+            [:div.container {:display :grid
+                        :grid-gap "20px"
+                        :grid-template-rows "auto 1fr auto"
+                        :grid-template-columns "repeat(24, [col-start] 1fr)"}]
+            [:ul {:list-style-type "none"
+                  :padding 0}]
+            (at-media {:min-width "320px"}
+                      [:html
+                       {:font-size "calc(18px + 6 * ((100vw - 320px) / 680))"}])])
 
 (defn render
   [content]
   (html5 [:head [:meta {:charset "utf-8"}]
           (include-css "/css/codemirror.css")
-          (include-css "/css/blog.css")
-          (include-js "/js/scripts/klipse.js")]
-         async-fonts
-
-         [:div.foo (-> content :entry :content)
+          (include-js "/js/scripts/klipse.js")
+          [:style (css style)]
+          async-fonts]
+         [:div.container [:div.content (-> content :entry :content)]
           (include-js "/js/klipse_plugin.js")]))
